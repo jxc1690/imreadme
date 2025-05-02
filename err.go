6277c47a389d30fe481dacedc07375a2188b64b5
jxc1690 <jxc1690@qq.com>
2025-05-02 19:49:55 +0800

    修复bug

---
+++ err.go
@@ -0,0 +1,25 @@
+package response
+
+type ResErr int
+
+const (
+	SUCCESS         ResErr = iota
+	ErrOther               //未知错误
+	ErrTokenExpired        //登录已失效或者已过期
+	ErrPushData            //参数错误
+	ErrWhere               //查询错误
+	ErrUpData              //更新错误
+	ErrNoUser              //不是自己发送的消息
+	ErrNoSend              //发送失败
+	ErrNoRet               //撤回失败
+
+)
+
+const (
+	ErrNoCode  ResErr = iota + 100 //验证码错误
+	ErrGetCode                     //获取验证码失败
+
+	ErrUpFile ResErr = iota + 110 //上传文件失败
+
+	ErrAddFriend ResErr = iota + 120 //添加好友失败
+)
