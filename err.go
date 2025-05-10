712acf980a83cff171a08738cc7cc3fcc6d7caa4
jxc1690 <jxc1690@qq.com>
2025-05-10 15:16:42 +0800

    错误码更新

--- err.go
+++ err.go
@@ -12,14 +12,22 @@ const (
 	ErrNoUser              //不是自己发送的消息
 	ErrNoSend              //发送失败
 	ErrNoRet               //撤回失败
-
+	ErrUpFile              //上传文件失败
 )
 
 const (
 	ErrNoCode  ResErr = iota + 100 //验证码错误
 	ErrGetCode                     //获取验证码失败
+)
+const (
+	ErrUserNoWhere ResErr = iota + 200 //用户不存在
+)
 
-	ErrUpFile ResErr = iota + 110 //上传文件失败
-
-	ErrAddFriend ResErr = iota + 120 //添加好友失败
+const (
+	ErrFriend     ResErr = iota + 300 //添加好友失败
+	ErrFriendOver                     //已经是好友
+	ErrFriendMore                     //好友已满
+)
+const (
+	ErrGroup ResErr = iota + 400 //群组错误
 )
